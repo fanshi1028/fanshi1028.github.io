401daddb1de68d70dc03f1f090b9c0e6e0a1c298
fanshi <jackychany321@gmail.com>
2023-08-05 19:50:29 +0800

    fix build: paths are case sensitive in the action

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -1,6 +1,6 @@
 import { error } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
-import { existsSync } from "node:fs"
+import { readdirSync } from "node:fs"
 import { resolve } from "node:path"
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
@@ -17,14 +17,17 @@ const export_org_as_html = async (org_path: string, emacs_exe_path = "emacs") =>
   return stdout
 })
 
-export const load: PageServerLoad = async ({ params, depends }) => {
-  const slug = params.slug.toLowerCase()
-  depends(`org_post_update:${slug}`)
-  const path = resolve(`src/routes/posts/${slug}.org`)
-  if (existsSync(path)) {
+export const load: PageServerLoad = ({ params, depends }) => {
+  const ORG_POSTS_DIR = "src/routes/posts"
+  const path = readdirSync(ORG_POSTS_DIR).reduce<string | null>(
+    (acc, path) =>
+      acc || (path.toLowerCase() == `${params.slug}.org`.toLowerCase() && path) || null
+    , null)
+  depends(`org_post_update:${params.slug}`)
+  if (path) {
     return {
-      slug,
-      html: export_org_as_html(path)
+      slug: params.slug,
+      html: export_org_as_html(resolve(ORG_POSTS_DIR, path))
     }
   }
   else throw error(404)
