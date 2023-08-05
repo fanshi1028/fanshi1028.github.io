0ada5009845df085a8e9c3642d1e470214b5c69e
fanshi <jackychany321@gmail.com>
2023-08-05 18:46:50 +0800

    debug

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -1,31 +1,31 @@
 import { error } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
-import { existsSync } from "node:fs"
+import { existsSync, readFileSync } from "node:fs"
 import { resolve } from "node:path"
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
 
-const export_org_as_html = async (org_path: string, emacs_exe_path = "emacs") => promisify(exec)(`
-  ${emacs_exe_path} \
-  -batch ${org_path} \
-  -l ox \
-  --eval="(princ (org-export-as 'html nil nil t))"
-`).then(({ stderr, stdout }) => {
-  if (stderr != "") {
-    console.log(stderr)
-  }
-  return stdout
-})
+// const export_org_as_html = async (org_path: string, emacs_exe_path = "emacs") => promisify(exec)(`
+//   ${emacs_exe_path} \
+//   -batch ${org_path} \
+//   -l ox \
+//   --eval="(princ (org-export-as 'html nil nil t))"
+// `).then(({ stderr, stdout }) => {
+//   if (stderr != "") {
+//     console.log(stderr)
+//   }
+//   return stdout
+// })
 
 export const load: PageServerLoad = async ({ params, depends }) => {
   const slug = params.slug.toLowerCase()
   depends(`org_post_update:${slug}`)
   const path = resolve(`src/routes/posts/${slug}.org`)
-  if (existsSync(path)) {
-    return {
-      slug,
-      html: export_org_as_html(path)
-    }
+  // if (existsSync(path)) {
+  return {
+    slug,
+    html: readFileSync(path)
   }
-  else throw error(404, path)
+  // }
+  // else throw error(404, path)
 }
