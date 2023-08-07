c5773632a2d7a3a48765d4f502cdc7515fd1b723
fanshi <jackychany321@gmail.com>
2023-08-07 23:35:00 +0800

    refactor: org-html-head-include-default-style nil

--- src/lib/server/org_post.ts
+++ src/lib/server/org_post.ts
@@ -1,14 +1,31 @@
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
 
-export const export_org_as_html = async (org_path: string, emacs_exe_path = "emacs") => promisify(exec)(`
+const setq = (options: Record<string, any>) =>
+  Object.entries(options).reduce<string>(
+    (acc, i) => {
+      const val =
+        typeof i[1] == "boolean" ? (i[1] ? "t" : "nil")
+          : typeof i[1] == "string" ? `"${i[1]}"` :
+            i[1] ?? "nil"
+      return `${acc} --eval="(setq ${i[0]} ${val})"`
+    }, "")
+
+export const export_org_as_html = async (
+  org_path: string,
+  opts: Record<string, any> = {
+    "org-html-head-include-default-style": null
+  },
+  emacs_exe_path = "emacs"
+) => promisify(exec)(`
   ${emacs_exe_path} \
-  -batch ${org_path} \
-  -l ox \
-  --eval="(princ (org-export-as 'html nil nil t))"
-`).then(({ stderr, stdout }) => {
-    if (stderr != "") {
-        console.log(stderr)
-    }
-    return stdout
+-batch ${org_path} \
+-l ox \
+${setq(opts)} \
+--eval="(princ (org-export-as 'html))"
+  `).then(({ stderr, stdout }) => {
+  if (stderr != "") {
+    console.log(stderr)
+  }
+  return stdout
 })
