bb46ff6ff7f98c1fbb705d55af1e9f46372e40cc
fanshi1028 <jackychany321@gmail.com>
2025-09-03 19:39:33 +0800

    init

---
+++ app/Clock.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Clock where
+
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
+import Control.Monad.IO.Class (liftIO)
+import Data.Fixed
+import Data.Proxy
+import Data.Time
+import GHC.Float
+import Miso
+import Miso.Html
+import Miso.Html.Property
+import Miso.Lens
+
+data Model = Model
+  { _active :: Bool,
+    _timeLeft :: DiffTime,
+    _lastTick :: Maybe Double
+  }
+  deriving (Eq)
+
+active :: Lens Model Bool
+active = lens _active $ \record x -> record {_active = x}
+
+lastTick :: Lens Model (Maybe Double)
+lastTick = lens _lastTick $ \record x -> record {_lastTick = x}
+
+timeLeft :: Lens Model DiffTime
+timeLeft = lens _timeLeft $ \record x -> record {_timeLeft = x}
+
+data Action = Tick Double | Start | Stop
+
+updateModel :: Action -> Effect parent Model Action
+updateModel = \case
+  Tick t ->
+    use active >>= \case
+      False -> pure ()
+      True -> do
+        use lastTick >>= \case
+          Nothing -> pure ()
+          Just lt -> do
+            -- NOTE: https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
+            let milliToPico = (* fromIntegral (resolution (Proxy @E12) `div` resolution (Proxy @E3)))
+                diff = picosecondsToDiffTime . fromIntegral . double2Int . milliToPico $ t - lt
+            timeLeft %= max 0 . subtract diff
+        -- io_ $ consoleLog $ ms $ show diff
+        lastTick .= Just t
+  Start -> active .= True
+  Stop -> active .= False
+
+viewModel :: Model -> View Model Action
+viewModel m =
+  div_ [class_ "flex flex-col items-center gap-6 p-6"] $
+    let timeDisplay = text $ ms (formatTime defaultTimeLocale "%M:%00ES" $ m ^. timeLeft)
+     in if m ^. timeLeft <= 0
+          then [p_ [class_ "text-neutral-200"] ["Time's up"]]
+          else
+            if m ^. active
+              then
+                [ p_ [class_ "text-neutral-200 text-9xl"] [timeDisplay],
+                  -- button_ [onClick Stop, class_ "text-neutral-200 text-4xl"] ["Stop"]
+                  button_ [onClick Stop, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2"] ["Stop"]
+                  -- div_
+                  --   [class_ "flex flex-row justify-around w-full"]
+                  --   [ button_ [onClick Stop, class_ "text-neutral-200 text-4xl rounded px-4 py-2"] ["Stop"],
+                  --     button_ [onClick Stop, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2"] ["Stop"]
+                  --   ]
+                ]
+              else
+                [ p_ [class_ "text-neutral-200 text-9xl"] [timeDisplay],
+                  -- button_ [onClick Start, class_ "text-neutral-200 text-4xl"] ["Start"]
+                  button_ [onClick Start, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2 self-bew"] ["Start"]
+                ]
+
+stopWatch :: DiffTime -> Component parent Model Action
+stopWatch initTime =
+  (component (Model False initTime Nothing) updateModel viewModel)
+    { subs =
+        [ \sink -> forever $ do
+            liftIO . threadDelay . double2Int $ 0.1 * fromIntegral (resolution (Proxy @E6))
+            now >>= sink . Tick
+        ]
+    }
