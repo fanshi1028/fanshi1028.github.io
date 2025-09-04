63ab8a8db96eca3cb6e54eea06f053e68a474cf4
fanshi1028 <jackychany321@gmail.com>
2025-09-04 18:12:31 +0800

    implement (|>|) button

--- app/Clock.hs
+++ app/Clock.hs
@@ -7,6 +7,7 @@ module Clock where
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
+import Data.Aeson hiding ((.=))
 import Data.Fixed
 import Data.Proxy
 import Data.Time
@@ -16,6 +17,7 @@ import Miso.Html
 import Miso.Html.Property
 import Miso.Lens
 import Miso.String
+import Prelude hiding (error)
 
 data Model = Model
   { _active :: Bool,
@@ -33,23 +35,27 @@ lastTick = lens _lastTick $ \record x -> record {_lastTick = x}
 timeLeft :: Lens Model DiffTime
 timeLeft = lens _timeLeft $ \record x -> record {_timeLeft = x}
 
-data Action = Tick Double | Start | Stop
+data Action = Tick Double | Start | Stop | End
 
 data StopWatchTickSub = StopWatchTickSub
 
 instance ToMisoString StopWatchTickSub where
   toMisoString _ = "StopWatchTickSub"
 
-updateModel :: Action -> Effect parent Model Action
-updateModel = \case
-  Tick t -> do
-    lastTick <<.= Just t >>= \case
-      Nothing -> pure ()
-      Just lt -> do
-        -- NOTE: https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
-        let milliToPico = (* fromIntegral (resolution (Proxy @E12) `div` resolution (Proxy @E3)))
-            diff = picosecondsToDiffTime . truncateDoubleInteger . milliToPico $ t - lt
-        timeLeft %= max 0 . subtract diff
+updateModel :: (ToJSON message) => message -> Action -> Effect parent Model Action
+updateModel endMessageToParent = \case
+  End -> mailParent endMessageToParent
+  Tick t ->
+    use timeLeft >>= \case
+      0 -> issue End
+      timeleft' ->
+        lastTick <<.= Just t >>= \case
+          Nothing -> pure ()
+          Just lt -> do
+            -- NOTE: https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
+            let milliToPico = (* fromIntegral (resolution (Proxy @E12) `div` resolution (Proxy @E3)))
+                diff = picosecondsToDiffTime . truncateDoubleInteger . milliToPico $ t - lt
+            timeLeft .= max 0 (timeleft' - diff)
   Start -> do
     active .= True
     startSub StopWatchTickSub $ \sink -> forever $ do
@@ -64,22 +70,16 @@ viewModel :: Model -> View Model Action
 viewModel m =
   div_ [class_ "flex flex-col items-center gap-6 p-6"] $
     let timeDisplay = text $ ms (formatTime defaultTimeLocale "%M:%00ES" $ m ^. timeLeft)
-     in if m ^. timeLeft <= 0
-          then [p_ [class_ "text-neutral-200"] ["Time's up"]]
+     in if m ^. active
+          then
+            [ p_ [class_ "text-neutral-200 text-9xl"] [timeDisplay],
+              div_
+                [class_ "flex flex-row justify-around w-full"]
+                [ button_ [onClick Stop, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2"] ["Stop"],
+                  button_ [onClick End, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2"] ["|>|"]
+                ]
+            ]
           else
-            if m ^. active
-              then
-                [ p_ [class_ "text-neutral-200 text-9xl"] [timeDisplay],
-                  -- button_ [onClick Stop, class_ "text-neutral-200 text-4xl"] ["Stop"]
-                  button_ [onClick Stop, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2"] ["Stop"]
-                  -- div_
-                  --   [class_ "flex flex-row justify-around w-full"]
-                  --   [ button_ [onClick Stop, class_ "text-neutral-200 text-4xl rounded px-4 py-2"] ["Stop"],
-                  --     button_ [onClick Stop, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2"] ["Stop"]
-                  --   ]
-                ]
-              else
-                [ p_ [class_ "text-neutral-200 text-9xl"] [timeDisplay],
-                  -- button_ [onClick Start, class_ "text-neutral-200 text-4xl"] ["Start"]
-                  button_ [onClick Start, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2 self-bew"] ["Start"]
-                ]
+            [ p_ [class_ "text-neutral-200 text-9xl"] [timeDisplay],
+              button_ [onClick Start, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2 self-bew"] ["Start"]
+            ]
