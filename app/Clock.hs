3f1e301288cb154f4e530c117b027bd558c5208a
fanshi1028 <jackychany321@gmail.com>
2025-09-04 19:37:56 +0800

    replace (|>|) with svg icon

--- app/Clock.hs
+++ app/Clock.hs
@@ -20,6 +20,8 @@ import Miso.Html
 import Miso.Html.Property
 import Miso.Lens
 import Miso.String
+import Miso.Svg.Element
+import Miso.Svg.Property hiding (path_)
 import Prelude hiding (error)
 
 data Model = Model
@@ -73,15 +75,20 @@ updateModel = \case
 
 viewModel :: Model -> View Model Action
 viewModel m =
-  div_ [class_ "flex flex-col items-center gap-6 p-6"] $
+  div_ [class_ "flex flex-col items-center gap-4 p-6"] $
     let timeDisplay = text $ ms (formatTime defaultTimeLocale "%M:%00ES" $ m ^. timeLeft)
      in if m ^. active
           then
             [ p_ [class_ "text-neutral-200 text-9xl"] [timeDisplay],
               div_
-                [class_ "flex flex-row justify-around w-full"]
+                [class_ "flex flex-row justify-around w-full items-center"]
                 [ button_ [onClick Stop, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2"] ["Stop"],
-                  button_ [onClick End, class_ "bg-neutral-200 text-neutral-600 text-4xl rounded px-4 py-2"] ["|>|"]
+                  button_
+                    [onClick End, class_ "text-neutral-200 text-4xl rounded px-4 py-2"]
+                    [ svg_
+                        [class_ "fill-current size-16", xmlns_ "http://www.w3.org/2000/svg", viewBox_ "0 0 24 24"]
+                        [path_ [d_ "M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z"]]
+                    ]
                 ]
             ]
           else
