43fa3988f7575593edf56673793d190dcf8acf24
fanshi1028 <jackychany321@gmail.com>
2025-09-06 20:28:13 +0800

    refactor to prepare further changes

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
-import qualified Clock
-import Data.Aeson
 import Data.FileEmbed (embedFileRelative)
 import Miso
+import Miso.Html.Element
+import Miso.Html.Property
 import qualified Pomodoro
 import Prelude hiding (rem, unlines)
 
@@ -19,26 +20,13 @@ foreign export javascript "hs_start" main :: IO ()
 main :: IO ()
 main = run $ startApp app
 
-app :: App Pomodoro.Model Pomodoro.Action
-app =
-  ( component
-      ( Pomodoro.Model
-          ( Pomodoro.PomodoroSettings
-              (Pomodoro.ValueWithValidation (ms $ show Pomodoro.defaultPomodoro) $ pure Pomodoro.defaultPomodoro)
-              (Pomodoro.ValueWithValidation (ms $ show Pomodoro.defaultShortBreak) $ pure Pomodoro.defaultShortBreak)
-              (Pomodoro.ValueWithValidation (ms $ show Pomodoro.defaultLongBreak) $ pure Pomodoro.defaultLongBreak)
-          )
-          False
-          []
-          Pomodoro.defaultCurrentPomodoro
-          Pomodoro.defaultPomodoroFutureQueue
-      )
-      Pomodoro.updateModel
-      Pomodoro.viewModel
-  )
-    { events = defaultEvents,
-      styles = [Style $ ms $(embedFileRelative "static/output.css")],
-      mailbox = \v -> case fromJSON v of
-        Error _ -> Nothing
-        Success Clock.ClockDoneMessage -> Just Pomodoro.Next
-    }
+app :: App () action
+app = component () noop $ \() ->
+  div_
+    []
+    [ div_ [id_ "pomodoro"]
+        +> Pomodoro.pomodoroComponent
+          { events = defaultEvents,
+            styles = [Style $ ms $(embedFileRelative "static/output.css")]
+          }
+    ]
