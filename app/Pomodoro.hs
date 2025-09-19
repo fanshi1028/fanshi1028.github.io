85753594ec247344ba44a7b0ddd531e15e3e42d9
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-09-19 18:16:19 +0800

    fix style and refactor

--- app/Pomodoro.hs
+++ app/Pomodoro.hs
@@ -219,84 +219,87 @@ updateModel = \case
       Right n -> validateAll [validateMultiple5, validateMin5, validateMax45] n
 
 viewModel :: Model -> View Model Action
-viewModel m = div_ [class_ "flex flex-col container items-center mx-auto justify-center pb-6 min-h-screen bg-neutral-200"] $ case m._pomodoroQueue of
-  [] ->
-    [ div_ [class_ "w-full absolute top-8"] $ case m._pomodoroPastQueue of
-        [] -> []
-        _ ->
-          [ h1_ [class_ "text-center text-lg text-neutral-600 font-bold px-6"] [text "Pomodoro Ended!"],
-            p_ [class_ "text-center prose text-neutral-600 px-4"] [text "Ready to start a new Pomodoro?"]
-          ],
-      settingsView True
-    ]
-  (_pomodoroTime -> currentPomodoroTime, _) : future ->
-    [ h1_ [class_ "sr-only"] [text "Pomodoro"],
-      button_ [onClick SwitchToPRD, class_ "sticky top-2 self-end mr-2 hover:animate-wiggle hover:[animation-delay:0.25s]"] [prdSwitchSVG "stroke-neutral-600 size-6 hover:size-8"],
-      let currentPomodoroView =
-            div_
-              [ class_ $ "absolute transition-transform duration-500" <> sizeCls,
-                styleInline_ $ "backface-visibility:hidden;" <> if m._settingsOpen then "transform:rotateY(180deg);" else ""
-              ]
-              [ button_
-                  [onClick $ SettingsOpen True, class_ "absolute top-4 right-4 hover:animate-wiggle"]
-                  [ p_ [class_ "sr-only"] ["Open Settings"],
-                    svg_
-                      [class_ "fill-none stroke-2 stroke-neutral-400 size-8 sm:size-10 hover:size-10", xmlns_ "http://www.w3.org/2000/svg", viewBox_ "0 0 24 24"]
-                      [ path_ [strokeLinecap_ "round", strokeLinejoin_ "round", d_ "M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"],
-                        path_ [strokeLinecap_ "round", strokeLinejoin_ "round", d_ "M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"]
-                      ]
-                  ],
-                div_
-                  [ key_ @MisoString $ "stopwatch " <> ms (show currentPomodoroTime),
-                    class_ "bg-neutral-600 rounded-lg shadow-lg shadow-neutral-600 h-full w-full pt-6"
-                  ]
-                  +> ( ( component
-                           (Clock.Model False currentPomodoroTime Nothing)
-                           Clock.updateModel
-                           Clock.viewModel
-                       )
-                         { initialAction = Just Clock.Start
-                         }
-                     )
-              ]
-       in div_
-            [class_ "contents"]
-            [ h2_ [class_ "sr-only"] [text "Pomodoro Queue"],
-              ul_ [class_ "flex flex-col md:flex-row md:justify-around items-center gap-3"] $
-                let pastItemView (i, idx) =
-                      li_
-                        [ class_ $
-                            "px-2 "
-                              <> ( case Map.lookup (PastItemTransition idx) m._transitionMap of
-                                     Nothing -> ""
-                                     Just False -> "transition-[transform,opacity] opacity-0 translate-y-16"
-                                     Just True -> "transition-[transform,opacity]"
-                                 )
-                        ]
-                        [pomodoroView (Just "text-neutral-400 font-semibold") i]
-                    futureItemView (i, idx) =
-                      li_
-                        [ class_ $
-                            "px-2 "
-                              <> ( case Map.lookup (FutureItemTransition idx) m._transitionMap of
-                                     Nothing -> ""
-                                     Just False -> "transition-[transform,opacity] -translate-y-16 opacity-0"
-                                     Just True -> "transition-[transform,opacity]"
-                                 )
-                        ]
-                        [pomodoroView (Just "text-neutral-500 font-semibold text-lg") i]
-                 in [ case m._pomodoroPastQueue of
-                        [] -> div_ [] []
-                        justPast : rest -> ul_ [class_ "contents md:flex md:flex-col md:items-center md:justify-start md:self-start md:basis-1/4"] $ foldl' (\acc i -> pastItemView i : acc) [pastItemView justPast] rest,
-                      div_ [class_ $ "relative my-6" <> sizeCls] [settingsView False, currentPomodoroView],
-                      case future of
-                        [] -> div_ [] []
-                        nearFuture : rest ->
-                          ul_
-                            [class_ "contents md:flex md:flex-col md:items-center md:justify-end md:self-end md:basis-1/4"]
-                            (futureItemView nearFuture : (futureItemView <$> rest))
+viewModel m =
+  div_ [class_ "flex flex-col container items-center mx-auto pb-6 sm:pb-8 min-h-dvh bg-neutral-200"] $
+    [ button_ [onClick SwitchToPRD, class_ "sticky self-end top-2 mr-2 hover:animate-wiggle hover:[animation-delay:0.25s]"] [prdSwitchSVG "stroke-neutral-600 size-6 sm:size-8 hover:size-8 hover:sm:size-10"],
+      div_ [class_ "contents"] $ case m._pomodoroQueue of
+        [] ->
+          [ div_ [class_ "mt-6"] $ case m._pomodoroPastQueue of
+              [] -> []
+              _ ->
+                [ h1_ [class_ "text-center text-lg text-neutral-600 font-bold px-6"] [text "Pomodoro Ended!"],
+                  p_ [class_ "text-center text-neutral-600 px-4"] [text "Ready to start a new Pomodoro?"]
+                ],
+            div_ [class_ $ "mx-auto mt-8" <> sizeCls] $ [settingsView True]
+          ]
+        (_pomodoroTime -> currentPomodoroTime, _) : future ->
+          [ h1_ [class_ "sr-only"] [text "Pomodoro"],
+            let currentPomodoroView =
+                  div_
+                    [ class_ $ "absolute transition-transform duration-500" <> sizeCls,
+                      styleInline_ $ "backface-visibility:hidden;" <> if m._settingsOpen then "transform:rotateY(180deg);" else ""
                     ]
-            ]
+                    [ button_
+                        [onClick $ SettingsOpen True, class_ "absolute top-4 right-4 hover:animate-wiggle"]
+                        [ p_ [class_ "sr-only"] ["Open Settings"],
+                          svg_
+                            [class_ "fill-none stroke-2 stroke-neutral-400 size-8 sm:size-10 hover:size-10", xmlns_ "http://www.w3.org/2000/svg", viewBox_ "0 0 24 24"]
+                            [ path_ [strokeLinecap_ "round", strokeLinejoin_ "round", d_ "M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"],
+                              path_ [strokeLinecap_ "round", strokeLinejoin_ "round", d_ "M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"]
+                            ]
+                        ],
+                      div_
+                        [ key_ @MisoString $ "stopwatch " <> ms (show currentPomodoroTime),
+                          class_ "bg-neutral-600 rounded-lg shadow-lg shadow-neutral-600 h-full w-full pt-6"
+                        ]
+                        +> ( ( component
+                                 (Clock.Model False currentPomodoroTime Nothing)
+                                 Clock.updateModel
+                                 Clock.viewModel
+                             )
+                               { initialAction = Just Clock.Start
+                               }
+                           )
+                    ]
+             in div_
+                  [class_ "contents"]
+                  [ h2_ [class_ "sr-only"] [text "Pomodoro Queue"],
+                    ul_ [class_ "flex flex-col md:flex-row md:justify-around items-center gap-3"] $
+                      let pastItemView (i, idx) =
+                            li_
+                              [ class_ $
+                                  "px-2 "
+                                    <> ( case Map.lookup (PastItemTransition idx) m._transitionMap of
+                                           Nothing -> ""
+                                           Just False -> "transition-[transform,opacity] opacity-0 translate-y-16"
+                                           Just True -> "transition-[transform,opacity]"
+                                       )
+                              ]
+                              [pomodoroView (Just "text-neutral-400 font-semibold") i]
+                          futureItemView (i, idx) =
+                            li_
+                              [ class_ $
+                                  "px-2 "
+                                    <> ( case Map.lookup (FutureItemTransition idx) m._transitionMap of
+                                           Nothing -> ""
+                                           Just False -> "transition-[transform,opacity] -translate-y-16 opacity-0"
+                                           Just True -> "transition-[transform,opacity]"
+                                       )
+                              ]
+                              [pomodoroView (Just "text-neutral-500 font-semibold text-lg") i]
+                       in [ case m._pomodoroPastQueue of
+                              [] -> div_ [] []
+                              justPast : rest -> ul_ [class_ "contents md:flex md:flex-col md:items-center md:justify-start md:self-start md:basis-1/4"] $ foldl' (\acc i -> pastItemView i : acc) [pastItemView justPast] rest,
+                            div_ [class_ $ "relative my-6" <> sizeCls] [settingsView False, currentPomodoroView],
+                            case future of
+                              [] -> div_ [] []
+                              nearFuture : rest ->
+                                ul_
+                                  [class_ "contents md:flex md:flex-col md:items-center md:justify-end md:self-end md:basis-1/4"]
+                                  (futureItemView nearFuture : (futureItemView <$> rest))
+                          ]
+                  ]
+          ]
     ]
   where
     sizeCls = " w-80 sm:w-96 h-64 sm:h-80"
