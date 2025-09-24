bd6ff816be9f4aca5bb150c7564731a4022c3cc5
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-09-24 11:03:00 +0800

    refactor, init Home

--- app/Main.hs
+++ app/Main.hs
@@ -4,24 +4,19 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
 #ifndef WASM
 import Data.FileEmbed
 #endif
-import Data.List.NonEmpty
 import GHC.Generics
 import Miso
 import Miso.Html
 import Miso.Router as Router
-import Network.URI.Static
-import qualified Pomodoro
-import ProductRequirementDocument as PRD
 import Prelude hiding (rem, unlines)
+import Home
 
 -----------------------------------------------------------------------------
 #ifdef WASM
@@ -31,68 +26,14 @@ foreign export javascript "hs_start" main :: IO ()
 
 main :: IO ()
 main = run $ miso $ \case
-  URI "" "" _ -> app
-  _ -> component () noop $ \() -> p_ [] ["TEMP FIXME 404"]
-
-data Route = Index | Pomodoro
-  deriving stock (Generic)
-  deriving anyclass (Router)
-
-app :: App () action
-app =
-  ( component () noop $ \() ->
-      div_ [] $
-        [ div_ [key_ @MisoString "prd"]
-            +> ( component
-                   (PRD.Model False Pomodoro.pomodoroPRD)
-                   PRD.updateModel
-                   PRD.viewModel
-               )
-              { initialAction = Just PRD.Subscribe
-              },
-          div_ [key_ @MisoString "pomodoro"] +> Pomodoro.pomodoroComponent
-        ]
-  )
-    {
+  URI "" "" _ -> home {
       events = defaultEvents
 #ifndef WASM
     , styles = [Style $ ms $(embedFileRelative "static/output.css")]
 #endif
     }
+  _ -> component () noop $ \() -> p_ [] ["TEMP FIXME 404"]
 
-sitePRD :: ProductRequirementDocument
-sitePRD =
-  ProductRequirementDocument
-    ( ProblemAlignment
-        ( Problem
-            "I need a personal site"
-            "As a programmer, I need a stronger online presence, to host my blog if I have anything to say, to host my profolio, etc"
-            "As an introvert, I am no good at marketing, this site and the online presence it built will stay and evolve and open door for opportunity that I may not forseen"
-            "Also the blog itself is showcasing all my programming skills from Frontend to Backend even other skills like Product Design, etc"
-            "It will host some tools that I will use, other could use, which potentially draw traffic."
-            "A playground to play with idea that interested me"
-            :| []
-        )
-        "The site will be built from features, one by one, and UI will come naturally. Features will be organised by tags, let's say our entry point will be a word cloud of tag? A collection of tools, blogpost, organised by tags, visitors guided by the flexible UI."
-        ( "Host tools that I or visitors could use"
-            :| ["UI should guide visitors what interested them smoothly", "UI should be simple, modern and not over engineered", "The site will be a testimonial of my skills and also my product sense and development philosophy"]
-        )
-    )
-    ( SolutionAlignment
-        "WorldCloudOfTag -> Features/Tools/BlogPost"
-        [ "Tag Word Cloud navigation, users could click on their interested tag on the word cloud to the corresponding content",
-          "Modular design, enable me to increasementally add new features with ease",
-          "Every new feature will be started with a PRD(Product Requirement Document), and the PRDs will be avaliable to users, users could see the PRD of a feature on this site if they want and switch back to the feature as they wish"
-        ]
-        [ OpenIssues "Tech Stack" $
-            "Use Miso and Tailwind, as I want to explore Miso, which makes powered by Web-Assembly(compiled from Haskell of course), later may turn it into apps via miso-lynx. tailwind is no brainer"
-              :| [ "The other choice could be a haskell backend + htmx, but github page is static and no backend",
-                   "Also Hyperbole(haskell lib) is also interesting, but it is not stable and again need a backend, not suitable for the use case here"
-                 ],
-          OpenIssues "Edge Computing, Worker" $ "Will expand into this territory and a necessary features of this site demands a backend, but not for now" :| []
-        ]
-        [ Reference (Just "Refactoring UI") [uri|https://www.refactoringui.com/|] $ "Following the ui philosophy in this book. feature first, then build the ui to suit the feature" :| [],
-          Reference (Just "Figma PRD") [uri|https://coda.io/@yuhki/figmas-approach-to-product-requirement-docs|] $ "I like this approach to writie product requirement docs" :| []
-        ]
-    )
-    []
+data Route = Index | Pomodoro
+  deriving stock (Generic)
+  deriving anyclass (Router)
--- prerender/Main.hs
+++ prerender/Main.hs
@@ -1,35 +1,24 @@
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main where
 
+import Home
 import Miso
 import Miso.Html.Element as Html
 import Miso.Html.Property
 import Miso.Html.Render
-import qualified Pomodoro
-import ProductRequirementDocument as PRD
 import System.File.OsPath as IO
 import System.OsPath
 
 main :: IO ()
 main =
-  IO.writeFile [osp|index.html|] . toHtml $
-    wrapHtml . div_ [] $
-      [ div_ [key_ @MisoString "prd"]
-          +> ( component
-                 (PRD.Model False Pomodoro.pomodoroPRD)
-                 PRD.updateModel
-                 PRD.viewModel
-             )
-            { initialAction = Just PRD.Subscribe
-            },
-        div_ [key_ @MisoString "pomodoro"] +> Pomodoro.pomodoroComponent
-      ]
+  IO.writeFile [osp|index.html|] . toHtml $ wrapHtml home
 
-wrapHtml :: View model action -> [View model action]
-wrapHtml innerView =
+wrapHtml :: App model action -> [View model action]
+wrapHtml app =
   [ doctype_,
     html_
       []
@@ -42,7 +31,7 @@ wrapHtml innerView =
           ],
         body_ [] $
           [ script_ [src_ "index.js", type_ "module"] "",
-            innerView
+            app.view app.model
           ]
       ]
   ]
