eccba93b52279e7acdb2313b880667d9e820eb35
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-09-25 10:54:13 +0800

    fixup! init styling for Home

--- src/Home.hs
+++ src/Home.hs
@@ -19,7 +19,7 @@ home =
       div_
         [class_ "flex flex-col items-center justify-center min-h-dvh bg-neutral-200"]
         [ h1_ [class_ "sr-only"] [text "Tools"],
-          ul_ [class_ "font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-2 sm:py-3 md:py-4 lg:py-6 xl:py-8 text-neutral-700 bg-neutral-400 shadow-md shadow-neutral-600 rounded-lg"] [button_ [onClick $ GotoRoute Pomodoro] ["Pomodoro"]]
+          ul_ [] [button_ [onClick $ GotoRoute Pomodoro, class_ "font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-2 sm:py-3 md:py-4 lg:py-6 xl:py-8 text-neutral-700 bg-neutral-400 shadow-md shadow-neutral-600 rounded-lg hover:animate-wiggle"] ["Pomodoro"]]
         ]
     ]
 
