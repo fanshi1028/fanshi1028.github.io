f09827826909d9596adce714989afeb4dcfe70ff
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-09-25 15:52:03 +0800

    fixup! refactor PRD render

--- src/Route.hs
+++ src/Route.hs
@@ -3,11 +3,11 @@
 
 module Route (Route (..), Action (GotoRoute), routerComponent) where
 
-import Data.Aeson hiding ((.=))
 import GHC.Generics
 import Miso
 import Miso.Lens
 import Miso.Router
+import ProductRequirementDocument
 
 data Route = Index | Pomodoro
   deriving stock (Eq, Show, Enum, Bounded, Generic)
@@ -20,17 +20,19 @@ data Action
 
 type Model = Either MisoString Route
 
-updateModel :: Action -> Effect parent Model Action
-updateModel = \case
+updateModel :: (Route -> ProductRequirementDocument) -> Action -> Effect parent Model Action
+updateModel routeToPRD = \case
   ServerError err -> this .= Left err
   GotoRoute uri -> do
     io_ . pushURI $ toURI uri
     issue $ SetURI uri
-  SetURI uri -> this .= Right uri
+  SetURI uri -> do
+    this .= Right uri
+    publish prdTopic $ routeToPRD uri
 
-routerComponent :: (Model -> View Model Action) -> Model -> Component parent Model Action
-routerComponent routeToView uri =
-  (component uri updateModel routeToView)
+routerComponent :: (Model -> View Model Action) -> (Route -> ProductRequirementDocument) -> Model -> Component parent Model Action
+routerComponent routerView routeToPRD uri =
+  (component uri (updateModel routeToPRD) routerView)
     { subs =
         [ routerSub $ \case
             Left err -> ServerError . ms $ show err
