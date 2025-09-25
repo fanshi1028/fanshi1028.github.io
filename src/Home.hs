a42380b55f19ea8e4af9635a727abbb51deced93
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-09-25 10:31:02 +0800

    init styling for Home

--- src/Home.hs
+++ src/Home.hs
@@ -17,12 +17,9 @@ home =
   div_ [] $
     [ div_ [key_ @MisoString "prd"] +> (prdComponent False sitePRD),
       div_
-        []
-        [ h1_ [] [text "Tools"],
-          ul_
-            []
-            [ button_ [onClick $ GotoRoute Pomodoro] ["Pomodoro"]
-            ]
+        [class_ "flex flex-col items-center justify-center min-h-dvh bg-neutral-200"]
+        [ h1_ [class_ "sr-only"] [text "Tools"],
+          ul_ [class_ "font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-2 sm:py-3 md:py-4 lg:py-6 xl:py-8 text-neutral-700 bg-neutral-400 shadow-md shadow-neutral-600 rounded-lg"] [button_ [onClick $ GotoRoute Pomodoro] ["Pomodoro"]]
         ]
     ]
 
