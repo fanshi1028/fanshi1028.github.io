c09a61f746ee7c5a7e4641f68b4408ab687a0766
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-09-26 12:36:05 +0800

    temp fix for js build runtime error: 'HEAP8' was not exported. add it to EXPORTED_RUNTIME_METHODS (see the Emscripten FAQ)

---
+++ temp-fix.js
@@ -0,0 +1,3 @@
+//#OPTIONS:EMCC:EXPORTED_RUNTIME_METHODS=HEAP8
+// NOTE: https://ghc.gitlab.haskell.org/ghc/doc/users_guide/javascript.html#emcc-pragmas
+// NOTE: https://gitlab.haskell.org/ghc/ghc/-/commit/2a411fc45d19a37615a6a47e0530c014c244bdf4
