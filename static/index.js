244ba42ba1b70d2697ab27a2c98b823584b7f610
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-09-28 21:51:19 +0800

    ts dev env + fix js

--- static/index.js
+++ static/index.js
@@ -11,9 +11,9 @@ import {
     simd,
     // optional wasm extensions not enabled
     // tailCall
-} from "./wasm-feature-detect/index.js";
+} from "./wasm-feature-detect/dist/esm/index.js";
 
-const wasm-feature-detections = await Promise.all([
+const wasm_feature_detections = await Promise.all([
     saturatedFloatToInt(),
     signExtensions(),
     mutableGlobals(),
@@ -23,34 +23,34 @@ const wasm-feature-detections = await Promise.all([
     simd(),
 ])
 
-if (wasm-feature-detections.every(i => i)) {
+if (wasm_feature_detections.every(i => i)) {
 
-    import { WASI, OpenFile, File, ConsoleStdout } from "./browser_wasi_shim/index.js";
-    import ghc_wasm_jsffi from "./ghc_wasm_jsffi.js";
-    
+    const { WASI, OpenFile, File, ConsoleStdout } = await import("./browser_wasi_shim/dist/index.js");
+    const ghc_wasm_jsffi = await import("./ghc_wasm_jsffi.js");
+
     const args = [];
     const env = ["GHCRTS=-H64m"];
     const fds = [
-      new OpenFile(new File([])), // stdin
-      ConsoleStdout.lineBuffered((msg) => console.log(`[WASI stdout] ''${msg}`)),
-      ConsoleStdout.lineBuffered((msg) => console.warn(`[WASI stderr] ''${msg}`)),
+        new OpenFile(new File([])), // stdin
+        ConsoleStdout.lineBuffered((msg) => console.log(`[WASI stdout] ''${msg}`)),
+        ConsoleStdout.lineBuffered((msg) => console.warn(`[WASI stderr] ''${msg}`)),
     ];
     const options = { debug: false };
     const wasi = new WASI(args, env, fds, options);
-    
+
     const instance_exports = {};
     const { instance } = await WebAssembly.instantiateStreaming(fetch("fanshi1028-site.wasm"), {
-      wasi_snapshot_preview1: wasi.wasiImport,
-      ghc_wasm_jsffi: ghc_wasm_jsffi(instance_exports),
+        wasi_snapshot_preview1: wasi.wasiImport,
+        ghc_wasm_jsffi: ghc_wasm_jsffi(instance_exports),
     });
     Object.assign(instance_exports, instance.exports);
-    
+
     wasi.initialize(instance);
     await instance.exports.hs_start(globalThis.example);
-    
-    
+
+
 } else {
-    import "all.js"
+    import("all.js")
 }
 
 
