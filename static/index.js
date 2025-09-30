bdce501e5d639e828b38a7a82c0bd44c59cdabee
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-09-30 18:30:57 +0800

    revert factor out wasm.js

--- static/index.js
+++ static/index.js
@@ -24,7 +24,30 @@ const wasm_feature_detections = await Promise.all([
 ])
 
 if (wasm_feature_detections.every(i => i)) {
-    await import("./wasm.js")
+    const browser_wasi_shim_imports = import("./browser_wasi_shim/dist/index.js");
+    const ghc_wasm_jsffi_imports = import("./ghc_wasm_jsffi.js");
+    
+    const args = [];
+    const env = ["GHCRTS=-H64m"];
+    
+    const { WASI, OpenFile, File, ConsoleStdout } = await browser_wasi_shim_imports
+    const fds = [
+        new OpenFile(new File([])), // stdin
+        ConsoleStdout.lineBuffered((msg) => console.log(`[WASI stdout] ''${msg}`)),
+        ConsoleStdout.lineBuffered((msg) => console.warn(`[WASI stderr] ''${msg}`)),
+    ];
+    const options = { debug: false };
+    const wasi = new WASI(args, env, fds, options);
+    
+    const instance_exports = {};
+    const { instance } = await WebAssembly.instantiateStreaming(fetch("fanshi1028-site.wasm"), {
+        wasi_snapshot_preview1: wasi.wasiImport,
+        ghc_wasm_jsffi: (await ghc_wasm_jsffi_imports).default(instance_exports),
+    });
+    Object.assign(instance_exports, instance.exports);
+    
+    wasi.initialize(instance);
+    await instance.exports.hs_start();
 } else {
     await import("./all.js")
 }
