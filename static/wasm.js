709be52125785ea18d59f81a66943cf4005f63d9
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-09-30 18:01:25 +0800

    chore: more "async"

--- static/wasm.js
+++ static/wasm.js
@@ -1,8 +1,10 @@
-const { WASI, OpenFile, File, ConsoleStdout } = await import("./browser_wasi_shim/dist/index.js");
-const { default: ghc_wasm_jsffi } = await import("./ghc_wasm_jsffi.js");
+const browser_wasi_shim_imports = import("./browser_wasi_shim/dist/index.js");
+const ghc_wasm_jsffi_imports = import("./ghc_wasm_jsffi.js");
 
 const args = [];
 const env = ["GHCRTS=-H64m"];
+
+const { WASI, OpenFile, File, ConsoleStdout } = await browser_wasi_shim_imports
 const fds = [
     new OpenFile(new File([])), // stdin
     ConsoleStdout.lineBuffered((msg) => console.log(`[WASI stdout] ''${msg}`)),
@@ -14,7 +16,7 @@ const wasi = new WASI(args, env, fds, options);
 const instance_exports = {};
 const { instance } = await WebAssembly.instantiateStreaming(fetch("fanshi1028-site.wasm"), {
     wasi_snapshot_preview1: wasi.wasiImport,
-    ghc_wasm_jsffi: ghc_wasm_jsffi(instance_exports),
+    ghc_wasm_jsffi: (await ghc_wasm_jsffi_imports).default(instance_exports),
 });
 Object.assign(instance_exports, instance.exports);
 
