fedd376c9b6dc8ca5274352607a47fead63fc394
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-10-15 11:35:42 +0800

    update PRD for Dashboard about UV Index isuues

--- src/ProductRequirementDocument/Dashboard.hs
+++ src/ProductRequirementDocument/Dashboard.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module ProductRequirementDocument.Dashboard (dashboardPRD) where
 
 import Data.List.NonEmpty
+import Network.URI.Static
 import ProductRequirementDocument
 
 dashboardPRD :: ProductRequirementDocument
@@ -26,11 +28,20 @@ dashboardPRD =
         )
     )
     ( SolutionAlignment
-        "Just a fixible and continuously evolving layout for a bunch of useful inforamtion"
+        "Just a fixable and continuously evolving layout for a bunch of useful information" --
         [ "User can expand item into details after glimping the most important information",
-          "Display curent/forcasted UV Index"
+          "Display curent/forecasted UV Index"
         ]
-        []
-        []
+        [ OpenIssues "Find some UV API to use" $
+            "At first I just found a random free api: https://currentuvindex.com but 1. its api use latitude and longitude but no city option to specify location which is not good for caching (and it has daily call limit). 2. and it works globally and I base in HK mostly currently"
+              :| [ "So I found one that specialise to HK: https://www.hko.gov.hk/en/weatherAPI/doc/files/HKO_Open_Data_API_Documentation.pdf",
+                   "Also we have https://dragon.best/api/glax_weather/ which support specifying city"
+                 ],
+          OpenIssues "Efficient API call/cache" $ "" :| ["We will use haxl to solve this!"]
+        ]
+        [ Reference (Just "CurrentUVIndex") [uri|https://currentuvindex.com|] $ "Just a free UV Index API" :| [],
+          Reference (Just "HKO Open Data API Doc (Weather API)") [uri|https://www.hko.gov.hk/en/weatherAPI/doc/files/HKO_Open_Data_API_Documentation.pdf|] $ "" :| [],
+          Reference (Just "About Haxl") [uri|https://engineering.fb.com/2014/06/10/web/open-sourcing-haxl-a-library-for-haskell/|] $ "" :| []
+        ]
     )
     []
