03507e6ea20a8284385209288c88c932e1fb455d
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-10-23 09:48:15 +0800

    simplify

--- src/Dashboard/DataSource/LocalStorage.hs
+++ src/Dashboard/DataSource/LocalStorage.hs
@@ -87,6 +87,9 @@ setLocalStorage k v = uncachedRequest $ SetLocalStorage k v
 removeLocalStorage :: Text -> GenHaxl JSContextRef w ()
 removeLocalStorage k = uncachedRequest $ RemoveLocalStorage k
 
+makeReqStorageKey :: (Typeable req, Show req) => req -> Text
+makeReqStorageKey req@(typeRepTyCon . typeOf -> tyCon) = pack $ tyConName tyCon <> ":" <> show req
+
 loadCacheFromLocalStorage :: (Serialise a, Typeable a, Eq a, Show a) => Text -> (a -> Bool) -> GenHaxl JSContextRef w a
 loadCacheFromLocalStorage key validateCache =
   getLocalStorage key >>= \case
@@ -94,8 +97,8 @@ loadCacheFromLocalStorage key validateCache =
       | validateCache res -> pure res
       | otherwise -> fail $ "LocalStorage Cache Invalidated: " <> unpack key
 
-fromLocalStorageOrDatafetch :: (Eq a, Typeable a, Show (req a), Serialise a, Request req a, DataSource JSContextRef req) => req a -> (a -> Bool) -> GenHaxl JSContextRef w a
-fromLocalStorageOrDatafetch req@(pack . show -> key) validateCache =
+fromLocalStorageOrDatafetch :: (Eq a, Typeable a, Serialise a, Request req a, DataSource JSContextRef req) => req a -> (a -> Bool) -> GenHaxl JSContextRef w a
+fromLocalStorageOrDatafetch req@(makeReqStorageKey -> key) validateCache =
   loadCacheFromLocalStorage key validateCache
     `catchAny` do
       r <- dataFetch req
