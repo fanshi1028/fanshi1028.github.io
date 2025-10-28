ed454bf89652e170a0abf30e12258bf894f827de
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-10-28 15:17:39 +0800

    fixup! init maplibre

--- src/MapLibre.hs
+++ src/MapLibre.hs
@@ -2,14 +2,14 @@ module MapLibre where
 
 import Control.Concurrent
 import Control.Monad
-import Control.Monad.Fix
 import Control.Monad.IO.Class
-import Data.Foldable
 import Language.Javascript.JSaddle.Object
 import Language.Javascript.JSaddle.Types
 import Language.Javascript.JSaddle.Value
 import Miso (consoleLog, toMisoString)
 import Miso.Navigator hiding (new)
+import UnliftIO.Async
+import UnliftIO.Exception
 
 mkMapLibreCfg :: JSString -> Geolocation -> JSM Object
 mkMapLibreCfg id' (Geolocation lat lon acc) = do
@@ -22,12 +22,13 @@ mkMapLibreCfg id' (Geolocation lat lon acc) = do
 
 createMapLibre :: JSString -> Geolocation -> JSM JSVal
 createMapLibre id' geo = do
-  maplibregl <- mfix $ \maplibregl -> do
-    jsg "maplibregl" >>= \maplibregl' ->
-      valIsUndefined maplibregl' >>= \case
-        False -> pure maplibregl'
-        True -> do
-          liftIO (threadDelay 150000)
-          consoleLog $ toMisoString "hi"
-          pure maplibregl
-  new (maplibregl ! "Map") [mkMapLibreCfg id' geo]
+  mapLibreMVar <- liftIO newEmptyMVar
+  withAsync
+    ( forever $ do
+        maplibregl <- jsg "maplibregl"
+        valIsUndefined maplibregl >>= \case
+          False -> new (maplibregl ! "Map") [mkMapLibreCfg id' geo] >>= liftIO . putMVar mapLibreMVar
+          True -> consoleLog $ toMisoString "hi"
+        liftIO (threadDelay 100000)
+    )
+    $ \ensureMaplibregl -> liftIO (readMVar mapLibreMVar) `finally` uninterruptibleCancel ensureMaplibregl
