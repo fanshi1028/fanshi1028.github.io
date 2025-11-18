0df10fbee8f26e22b52836358116e53263654b6f
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-11-18 12:13:18 +0800

    chores: simplify

--- src/Dashboard/DataSource/MisoRun.hs
+++ src/Dashboard/DataSource/MisoRun.hs
@@ -12,27 +12,25 @@ import Language.Javascript.JSaddle
 import Miso
 
 data MisoRunAction action a where
-  MisoRunAction :: action -> MisoRunAction action ()
+  MisoRunAction :: (Show action, Eq action) => action -> MisoRunAction action ()
 
-instance (Eq action) => Eq (MisoRunAction action a) where
-  (==) (MisoRunAction action1) (MisoRunAction action2) = action1 == action2
+deriving instance Eq (MisoRunAction action a)
 
-instance (Show action) => Show (MisoRunAction action a) where
-  show (MisoRunAction action) = "MisoRunAction " <> show action
+deriving instance Show (MisoRunAction action a)
 
-instance (Show action) => ShowP (MisoRunAction action) where showp = show
+instance ShowP (MisoRunAction action) where showp = show
 
 -- HACK: we don't care about the Hashable instance here, because we won't cache the result with `misoRunAction`
-instance (Eq action) => Hashable (MisoRunAction action a) where
+instance Hashable (MisoRunAction action a) where
   hashWithSalt s _ = hashWithSalt s (1 :: Int)
 
-instance (Typeable action) => DataSourceName (MisoRunAction action) where
+instance DataSourceName (MisoRunAction action) where
   dataSourceName _ = pack "MisoRunAction"
 
 instance (Typeable action) => StateKey (MisoRunAction action) where
   data State (MisoRunAction action) = MisoRunActionState JSContextRef (Sink action)
 
-instance (Typeable action, Show action, Eq action) => DataSource u (MisoRunAction action) where
+instance (Typeable action) => DataSource u (MisoRunAction action) where
   fetch (MisoRunActionState jscontext sink) _ _ =
     SyncFetch $ \reqs ->
       runJSaddle jscontext $
