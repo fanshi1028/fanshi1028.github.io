68b2843a6275686fe1a5121cd87fff339a11b3d8
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-11-22 14:35:35 +0800

    drops overlay from miso

--- flake.nix
+++ flake.nix
@@ -37,7 +37,6 @@
     }:
     let
       ghcVersion = "9122";
-      overlays = [ (import "${miso}/nix/overlay.nix") ];
       applyFixToHaxl =
         pkgs: haxl:
         nixpkgs.lib.pipe haxl (
@@ -77,6 +76,10 @@
               hself: hsuper:
               (
                 {
+                  miso =
+                    hself.callCabal2nixWithOptions "miso" (import "${miso}/nix/source.nix" pkgs).miso
+                      "-ftemplate-haskell"
+                      { };
                   haxl = applyFixToHaxl pkgs hsuper.haxl;
                   hashtables = hsuper.hashtables_1_4_2;
                   cborg = applyFixToCborg pkgs hsuper.cborg;
@@ -107,134 +110,122 @@
         );
     in
     {
-      packages = builtins.mapAttrs (
-        system: pkgs:
-        let
-          pkgsWithMisoOverlays = import nixpkgs { inherit system overlays; };
-        in
-        {
-          inherit (pkgs) tailwindcss closurecompiler;
+      packages = builtins.mapAttrs (system: pkgs: {
+        inherit (pkgs) tailwindcss closurecompiler;
 
-          miso = pkgsWithMisoOverlays.haskell.packages."ghc${ghcVersion}".miso;
-          haxl = applyFixToHaxl pkgs pkgsWithMisoOverlays.haskell.packages."ghc${ghcVersion}".haxl;
-          cborg = applyFixToCborg pkgs pkgsWithMisoOverlays.haskell.packages."ghc${ghcVersion}".cborg;
+        miso = pkgs.haskell.packages."ghc${ghcVersion}".miso; # TEMP FIXME
+        haxl = applyFixToHaxl pkgs pkgs.haskell.packages."ghc${ghcVersion}".haxl;
+        cborg = applyFixToCborg pkgs pkgs.haskell.packages."ghc${ghcVersion}".cborg;
 
-          prerender-js = mkDefaultPackage pkgsWithMisoOverlays {
-            modifier =
-              drv:
-              pkgs.lib.pipe drv [
-                (pkgs.haskell.lib.compose.setBuildTargets [ "prerender" ])
-                (pkgs.haskell.lib.compose.overrideCabal (_: {
-                  pname = "prerender";
-                }))
-              ];
-          };
+        prerender-js = mkDefaultPackage pkgs {
+          modifier =
+            drv:
+            pkgs.lib.pipe drv [
+              (pkgs.haskell.lib.compose.setBuildTargets [ "prerender" ])
+              (pkgs.haskell.lib.compose.overrideCabal (_: {
+                pname = "prerender";
+              }))
+            ];
+        };
 
-          prerender-wasm = mkDefaultPackage pkgsWithMisoOverlays {
-            modifier =
-              drv:
-              pkgs.lib.pipe drv [
-                (pkgs.haskell.lib.compose.setBuildTargets [ "prerender" ])
-                (pkgs.haskell.lib.compose.enableCabalFlag "prerender-wasm")
-                (pkgs.haskell.lib.compose.overrideCabal (_: {
-                  pname = "prerender";
-                }))
-              ];
-          };
-          fanshi1028-site-js = mkDefaultPackage pkgsWithMisoOverlays.pkgsCross.ghcjs {
-            overrides = hself: hsuper: ({
-              hashtables = pkgs.haskell.lib.enableCabalFlag hsuper.hashtables_1_4_2 "portable";
-              # NOTE: https://github.com/ghcjs/jsaddle/pull/162
-              jsaddle = pkgs.haskell.lib.appendPatch hsuper.jsaddle (
-                pkgs.fetchpatch {
-                  url = "https://patch-diff.githubusercontent.com/raw/ghcjs/jsaddle/pull/162.patch";
-                  hash = "sha256-jVaHy+7y4O6/jVx9CLIp/QHKRnL922ueLIGjP+Jd6b8=";
-                  stripLen = 1;
-                }
-              );
-            });
-            modifier =
-              drv:
-              pkgs.lib.pipe drv (
-                with pkgs.haskell.lib.compose;
-                [
-                  (enableCabalFlag "production")
-                  (setBuildTargets [ "exe:fanshi1028-site" ])
-                ]
-              );
-          };
-          browser_wasi_shim = pkgs.buildNpmPackage {
-            pname = "browser_wasi_shim";
-            version = "0.4.2";
-            src = browser_wasi_shim;
-            npmDepsHash = "sha256-5CUnps7UyX9U7ZRRaUy0t7lpXoOhFR8n7AEPTD0npF0=";
-          };
-          wasm-feature-detect = pkgs.buildNpmPackage {
-            pname = "wasm-feature-detect";
-            version = "1.8.0";
-            src = wasm-feature-detect;
-            npmDepsHash = "sha256-ikjjc7/MZRswwNsmSJC5KqLrNKbCbKYLQ9v87t1azTc=";
-          };
-        }
-      ) nixpkgs.legacyPackages;
+        prerender-wasm = mkDefaultPackage pkgs {
+          modifier =
+            drv:
+            pkgs.lib.pipe drv [
+              (pkgs.haskell.lib.compose.setBuildTargets [ "prerender" ])
+              (pkgs.haskell.lib.compose.enableCabalFlag "prerender-wasm")
+              (pkgs.haskell.lib.compose.overrideCabal (_: {
+                pname = "prerender";
+              }))
+            ];
+        };
+        fanshi1028-site-js = mkDefaultPackage pkgs.pkgsCross.ghcjs {
+          overrides = hself: hsuper: ({
+            hashtables = pkgs.haskell.lib.enableCabalFlag hsuper.hashtables_1_4_2 "portable";
+            # NOTE: https://github.com/ghcjs/jsaddle/pull/162
+            jsaddle = pkgs.haskell.lib.appendPatch hsuper.jsaddle (
+              pkgs.fetchpatch {
+                url = "https://patch-diff.githubusercontent.com/raw/ghcjs/jsaddle/pull/162.patch";
+                hash = "sha256-jVaHy+7y4O6/jVx9CLIp/QHKRnL922ueLIGjP+Jd6b8=";
+                stripLen = 1;
+              }
+            );
+          });
+          modifier =
+            drv:
+            pkgs.lib.pipe drv (
+              with pkgs.haskell.lib.compose;
+              [
+                (enableCabalFlag "production")
+                (setBuildTargets [ "exe:fanshi1028-site" ])
+              ]
+            );
+        };
+        browser_wasi_shim = pkgs.buildNpmPackage {
+          pname = "browser_wasi_shim";
+          version = "0.4.2";
+          src = browser_wasi_shim;
+          npmDepsHash = "sha256-5CUnps7UyX9U7ZRRaUy0t7lpXoOhFR8n7AEPTD0npF0=";
+        };
+        wasm-feature-detect = pkgs.buildNpmPackage {
+          pname = "wasm-feature-detect";
+          version = "1.8.0";
+          src = wasm-feature-detect;
+          npmDepsHash = "sha256-ikjjc7/MZRswwNsmSJC5KqLrNKbCbKYLQ9v87t1azTc=";
+        };
+      }) nixpkgs.legacyPackages;
 
-      devShells = builtins.mapAttrs (
-        system: pkgs:
-        let
-          pkgsWithMisoOverlays = import nixpkgs { inherit system overlays; };
-        in
-        {
-          without-build-tools = mkDefaultPackage pkgsWithMisoOverlays { returnShellEnv = true; };
-          default = mkDefaultPackage pkgsWithMisoOverlays {
-            overrides = hself: hsuper: {
-              jsaddle = pkgs.lib.pipe hsuper.jsaddle (
-                with pkgs.haskell.lib.compose;
-                [
-                  (enableCabalFlag "call-stacks")
-                  (enableCabalFlag "check-unchecked")
-                ]
-              );
-              hashtables = hsuper.hashtables_1_4_2;
-            };
-            modifier =
-              drv:
-              pkgs.haskell.lib.addBuildTools drv (
-                with pkgs;
-                [
-                  cabal-install
-                  tailwindcss
-                  ghciwatch
-                  # NOTE: tailwindcss_4 when trying to run
-                  # dyld: Symbol not found: _ubrk_clone
-                  #   Referenced from: /nix/store/2dxgd64421azhmwp63h9h3hzczgvh9w7-tailwindcss_4-4.1.7/bin/.tailwindcss-wrapped (which was built for Mac OS X 13.0)
-                  #   Expected in: /usr/lib/libicucore.A.dylib
-                  bun
-                  typescript
-                  typescript-language-server
-                  prettier
-                  closurecompiler
-                  # webpack-cli
-                  # swc
-                  uglify-js
-                  http-server
-                  (haskell-language-server.override {
-                    supportedGhcVersions = [ ghcVersion ];
-                    supportedFormatters = [ "ormolu" ];
-                  })
-                  emacs-lsp-booster
-                ]
-                ++ (with haskell.packages."ghc${ghcVersion}"; [
-                  cabal-gild_1_6_0_0
-                  ormolu_0_8_0_0
-                ])
-              );
-            returnShellEnv = true;
+      devShells = builtins.mapAttrs (system: pkgs: {
+        without-build-tools = mkDefaultPackage pkgs { returnShellEnv = true; };
+        default = mkDefaultPackage pkgs {
+          overrides = hself: hsuper: {
+            jsaddle = pkgs.lib.pipe hsuper.jsaddle (
+              with pkgs.haskell.lib.compose;
+              [
+                (enableCabalFlag "call-stacks")
+                (enableCabalFlag "check-unchecked")
+              ]
+            );
+            hashtables = hsuper.hashtables_1_4_2;
           };
-          wasm = pkgs.mkShell {
-            name = "The miso ${system} GHC WASM ${ghcVersion} shell";
-            packages = [ ghc-wasm.packages.${system}.all_9_12 ];
-          };
-        }
-      ) nixpkgs.legacyPackages;
+          modifier =
+            drv:
+            pkgs.haskell.lib.addBuildTools drv (
+              with pkgs;
+              [
+                cabal-install
+                tailwindcss
+                ghciwatch
+                # NOTE: tailwindcss_4 when trying to run
+                # dyld: Symbol not found: _ubrk_clone
+                #   Referenced from: /nix/store/2dxgd64421azhmwp63h9h3hzczgvh9w7-tailwindcss_4-4.1.7/bin/.tailwindcss-wrapped (which was built for Mac OS X 13.0)
+                #   Expected in: /usr/lib/libicucore.A.dylib
+                bun
+                typescript
+                typescript-language-server
+                prettier
+                closurecompiler
+                # webpack-cli
+                # swc
+                uglify-js
+                http-server
+                (haskell-language-server.override {
+                  supportedGhcVersions = [ ghcVersion ];
+                  supportedFormatters = [ "ormolu" ];
+                })
+                emacs-lsp-booster
+              ]
+              ++ (with haskell.packages."ghc${ghcVersion}"; [
+                cabal-gild_1_6_0_0
+                ormolu_0_8_0_0
+              ])
+            );
+          returnShellEnv = true;
+        };
+        wasm = pkgs.mkShell {
+          name = "The miso ${system} GHC WASM ${ghcVersion} shell";
+          packages = [ ghc-wasm.packages.${system}.all_9_12 ];
+        };
+      }) nixpkgs.legacyPackages;
     };
 }
