ab7268c7c461041e04421b3af9cfd82c70a5c75d
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-11-22 15:12:09 +0800

    factor out nix/haskell-overrides.nix

--- flake.nix
+++ flake.nix
@@ -37,32 +37,7 @@
     }:
     let
       ghcVersion = "9122";
-      applyFixToHaxl =
-        pkgs: haxl:
-        nixpkgs.lib.pipe haxl (
-          with pkgs.haskell.lib.compose;
-          [
-            unmarkBroken
-            (overrideCabal (drv: {
-              # NOTE: https://github.com/facebook/Haxl/issues/165
-              postPatch = ''
-                substituteInPlace Setup.hs --replace-fail Setup Main
-                sed -i 's/time >= 1.4 \&\& < 1.13/time >= 1.4 \&\& < 1.15/g' haxl.cabal
-              '';
-            }))
-          ]
-        );
-      applyFixToCborg =
-        pkgs: pkg:
-        nixpkgs.lib.pipe pkg (
-          with pkgs.haskell.lib.compose;
-          [
-            (overrideSrc { src = "${cborg}/cborg"; })
-            (overrideCabal (drv: {
-              patches = [ ];
-            }))
-          ]
-        );
+      make-haskell-overrides = pkgs: import ./nix/haskell-overrides.nix { inherit pkgs miso; };
 
       mkDefaultPackage =
         pkgs: args:
@@ -75,35 +50,7 @@
             overrides =
               hself: hsuper:
               (
-                {
-                  miso =
-                    hself.callCabal2nixWithOptions "miso" (import "${miso}/nix/source.nix" pkgs).miso
-                      "-ftemplate-haskell"
-                      { };
-                  haxl = applyFixToHaxl pkgs hsuper.haxl;
-                  hashtables = hsuper.hashtables_1_4_2;
-                  cborg = applyFixToCborg pkgs hsuper.cborg;
-                  statistics = hself.callHackageDirect {
-                    pkg = "statistics";
-                    ver = "0.16.4.0";
-                    sha256 = "sha256-BmFcx40Dvazu3fdbZJXLGyB3eNSZ0EZzSkK3cQKdSKo=";
-                  } { };
-                  dataframe = hself.callHackageDirect {
-                    pkg = "dataframe";
-                    ver = "0.3.3.6";
-                    sha256 = "sha256-4/O93bE21wTxN/LNWpDmr17o3bo+Xhq1qoB8qG6cq+E=";
-                  } { };
-                  granite = hself.callHackageDirect {
-                    pkg = "granite";
-                    ver = "0.3.0.5";
-                    sha256 = "sha256-QLxahMrjQ2tbXeQ0CBn2k5o0tRUgjyFy6EDJFon4T0Y=";
-                  } { };
-                  snappy-hs = hself.callHackageDirect {
-                    pkg = "snappy-hs";
-                    ver = "0.1.0.4";
-                    sha256 = "sha256-c9kgHvHYTcMI8Y2jeF2Emazm2lbqDIw+OABjd2VmXJM=";
-                  } { };
-                }
+                make-haskell-overrides pkgs hself hsuper
                 // (if args ? overrides then args.overrides hself hsuper else { })
               );
           }
@@ -112,10 +59,14 @@
     {
       packages = builtins.mapAttrs (system: pkgs: {
         inherit (pkgs) tailwindcss closurecompiler;
-
-        miso = pkgs.haskell.packages."ghc${ghcVersion}".miso; # TEMP FIXME
-        haxl = applyFixToHaxl pkgs pkgs.haskell.packages."ghc${ghcVersion}".haxl;
-        cborg = applyFixToCborg pkgs pkgs.haskell.packages."ghc${ghcVersion}".cborg;
+        inherit
+          (pkgs.haskell.packages."ghc${ghcVersion}".override {
+            overrides = make-haskell-overrides pkgs;
+          })
+          miso
+          haxl
+          cborg
+          ;
 
         prerender-js = mkDefaultPackage pkgs {
           modifier =
---
+++ nix/haskell-overrides.nix
@@ -0,0 +1,51 @@
+{ pkgs, miso }:
+hself: hsuper: {
+  miso =
+    hself.callCabal2nixWithOptions "miso" (import "${miso}/nix/source.nix" pkgs).miso
+      "-ftemplate-haskell"
+      { };
+  haxl = pkgs.lib.pipe hsuper.haxl (
+    with pkgs.haskell.lib.compose;
+    [
+      unmarkBroken
+      (overrideCabal (drv: {
+        # NOTE: https://github.com/facebook/Haxl/issues/165
+        postPatch = ''
+          substituteInPlace Setup.hs --replace-fail Setup Main
+          sed -i 's/time >= 1.4 \&\& < 1.13/time >= 1.4 \&\& < 1.15/g' haxl.cabal
+        '';
+      }))
+    ]
+  );
+  hashtables = hsuper.hashtables_1_4_2;
+  cborg = pkgs.lib.pipe hsuper.cborg (
+    with pkgs.haskell.lib.compose;
+    [
+      (overrideSrc { src = "${cborg}/cborg"; })
+      (overrideCabal (drv: {
+        patches = [ ];
+      }))
+    ]
+  );
+  statistics = hself.callHackageDirect {
+    pkg = "statistics";
+    ver = "0.16.4.0";
+    sha256 = "sha256-BmFcx40Dvazu3fdbZJXLGyB3eNSZ0EZzSkK3cQKdSKo=";
+  } { };
+  dataframe = hself.callHackageDirect {
+    pkg = "dataframe";
+    ver = "0.3.3.6";
+    sha256 = "sha256-4/O93bE21wTxN/LNWpDmr17o3bo+Xhq1qoB8qG6cq+E=";
+  } { };
+  granite = hself.callHackageDirect {
+    pkg = "granite";
+    ver = "0.3.0.5";
+    sha256 = "sha256-QLxahMrjQ2tbXeQ0CBn2k5o0tRUgjyFy6EDJFon4T0Y=";
+  } { };
+  snappy-hs = hself.callHackageDirect {
+    pkg = "snappy-hs";
+    ver = "0.1.0.4";
+    sha256 = "sha256-c9kgHvHYTcMI8Y2jeF2Emazm2lbqDIw+OABjd2VmXJM=";
+  } { };
+
+}
