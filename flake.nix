3a841b41a42dfcd372958fe30b344e48ac716bde
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-11-22 20:01:10 +0800

    refactor: init wasm-entry (ts)

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
     # copied from miso's flake
     nixpkgs.url = "github:nixos/nixpkgs?rev=9e2e8a7878573d312db421d69e071690ec34e98c";
     ghc-wasm.url = "gitlab:haskell-wasm/ghc-wasm-meta?host=gitlab.haskell.org";
-    browser_wasi_shim = {
-      url = "github:bjorn3/browser_wasi_shim?ref=v0.4.2";
-      flake = false;
-    };
-    wasm-feature-detect = {
-      url = "github:GoogleChromeLabs/wasm-feature-detect?ref=v1.8.0";
-      flake = false;
-    };
   };
 
   outputs =
@@ -22,8 +14,6 @@
       self,
       nixpkgs,
       ghc-wasm,
-      browser_wasi_shim,
-      wasm-feature-detect,
     }:
     let
       ghcVersion = "9122";
@@ -104,6 +94,7 @@
               }))
             ];
         };
+
         fanshi1028-site-js = mkDefaultPackage pkgs.pkgsCross.ghcjs {
           overrides = hself: hsuper: ({
             hashtables = pkgs.haskell.lib.enableCabalFlag hsuper.hashtables "portable";
@@ -125,18 +116,6 @@
                 (setBuildTargets [ "exe:fanshi1028-site" ])
               ]
             );
-        };
-        browser_wasi_shim = pkgs.buildNpmPackage {
-          pname = "browser_wasi_shim";
-          version = "0.4.2";
-          src = browser_wasi_shim;
-          npmDepsHash = "sha256-5CUnps7UyX9U7ZRRaUy0t7lpXoOhFR8n7AEPTD0npF0=";
-        };
-        wasm-feature-detect = pkgs.buildNpmPackage {
-          pname = "wasm-feature-detect";
-          version = "1.8.0";
-          src = wasm-feature-detect;
-          npmDepsHash = "sha256-ikjjc7/MZRswwNsmSJC5KqLrNKbCbKYLQ9v87t1azTc=";
         };
       }) nixpkgs.legacyPackages;
 
@@ -166,8 +145,6 @@
                 #   Referenced from: /nix/store/2dxgd64421azhmwp63h9h3hzczgvh9w7-tailwindcss_4-4.1.7/bin/.tailwindcss-wrapped (which was built for Mac OS X 13.0)
                 #   Expected in: /usr/lib/libicucore.A.dylib
                 bun
-                typescript
-                typescript-language-server
                 prettier
                 closurecompiler
                 # webpack-cli
@@ -190,6 +167,28 @@
         wasm = pkgs.mkShell {
           name = "The miso ${system} GHC WASM ${ghcVersion} shell";
           packages = [ ghc-wasm.packages.${system}.all_9_12 ];
+        };
+        npm = {
+          wasm-entry =
+            let
+              nodejs = pkgs.nodejs_24;
+            in
+            pkgs.mkShell {
+              packages = [
+                nodejs
+              ]
+              ++ (with pkgs; [
+                importNpmLock.hooks.linkNodeModulesHook
+                typescript
+                typescript-language-server
+                emacs-lsp-booster
+                prettier
+              ]);
+              npmDeps = pkgs.importNpmLock.buildNodeModules {
+                npmRoot = ./typescript/wasm-entry;
+                inherit nodejs;
+              };
+            };
         };
       }) nixpkgs.legacyPackages;
     };
