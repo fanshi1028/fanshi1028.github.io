b1557bb92707bd71dedb9fc2ce47970efd1901b6
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-11-23 17:03:31 +0800

    chore: use assert

--- nix/fanshi1028-site.nix
+++ nix/fanshi1028-site.nix
@@ -14,67 +14,65 @@
   wasm ? false,
   ...
 }@args:
-if wasm && !prerender then
-  lib.abort "nix build for wasm site is not supported yet"
-else
-  haskell.packages."ghc${ghcVersion}".developPackage (
-    {
-      source-overrides = callPackage ./haskell-source-overrides.nix { };
-    }
-    // (builtins.removeAttrs args [
-      "ghcVersion"
-      "prerender"
-      "wasm"
-    ])
-    // {
-      overrides = lib.composeManyExtensions [
-        (
-          if stdenv.hostPlatform == "ghcjs" && !prerender then
-            hself: hsuper: ({
-              hashtables = haskell.lib.enableCabalFlag hsuper.hashtables "portable";
-              # NOTE: https://github.com/ghcjs/jsaddle/pull/162
-              jsaddle = haskell.lib.appendPatch hsuper.jsaddle (fetchpatch {
-                url = "https://patch-diff.githubusercontent.com/raw/ghcjs/jsaddle/pull/162.patch";
-                hash = "sha256-jVaHy+7y4O6/jVx9CLIp/QHKRnL922ueLIGjP+Jd6b8=";
-                stripLen = 1;
-              });
-            })
-          else
-            hself: hsuper: { }
-        )
-        (callPackage ./haskell-overrides.nix { })
-        overrides
-      ];
+assert wasm -> prerender; # NOTE nix build for wasm site is not supported yet
+haskell.packages."ghc${ghcVersion}".developPackage (
+  {
+    source-overrides = callPackage ./haskell-source-overrides.nix { };
+  }
+  // (builtins.removeAttrs args [
+    "ghcVersion"
+    "prerender"
+    "wasm"
+  ])
+  // {
+    overrides = lib.composeManyExtensions [
+      (
+        if stdenv.hostPlatform == "ghcjs" && !prerender then
+          hself: hsuper: ({
+            hashtables = haskell.lib.enableCabalFlag hsuper.hashtables "portable";
+            # NOTE: https://github.com/ghcjs/jsaddle/pull/162
+            jsaddle = haskell.lib.appendPatch hsuper.jsaddle (fetchpatch {
+              url = "https://patch-diff.githubusercontent.com/raw/ghcjs/jsaddle/pull/162.patch";
+              hash = "sha256-jVaHy+7y4O6/jVx9CLIp/QHKRnL922ueLIGjP+Jd6b8=";
+              stripLen = 1;
+            });
+          })
+        else
+          hself: hsuper: { }
+      )
+      (callPackage ./haskell-overrides.nix { })
+      overrides
+    ];
 
-      modifier =
-        drv:
-        modifier (
-          lib.pipe drv (
-            with haskell.lib.compose;
-            if prerender then
-              if wasm then
-                [
-                  (setBuildTargets [ "prerender" ])
-                  (enableCabalFlag "prerender-wasm")
-                  (overrideCabal (_: {
-                    pname = "prerender";
-                  }))
-                ]
-              else
-                [
-                  (setBuildTargets [ "prerender" ])
-                  (overrideCabal (_: {
-                    pname = "prerender";
-                  }))
-                ]
-            else if wasm then
-              [ ] # NOTE: impossible case
+    modifier =
+      drv:
+      modifier (
+        lib.pipe drv (
+          with haskell.lib.compose;
+          if prerender then
+            if wasm then
+              [
+                (setBuildTargets [ "prerender" ])
+                (enableCabalFlag "prerender-wasm")
+                (overrideCabal (_: {
+                  pname = "prerender";
+                }))
+              ]
             else
               [
-                (enableCabalFlag "production")
-                (setBuildTargets [ "exe:fanshi1028-site" ])
+                (setBuildTargets [ "prerender" ])
+                (overrideCabal (_: {
+                  pname = "prerender";
+                }))
               ]
-          )
-        );
-    }
-  )
+          else if wasm then
+            [ ] # NOTE: impossible case
+          else
+            [
+              (enableCabalFlag "production")
+              (setBuildTargets [ "exe:fanshi1028-site" ])
+            ]
+        )
+      );
+  }
+)
