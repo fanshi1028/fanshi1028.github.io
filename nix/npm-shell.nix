6e2ead4aa0b345cb09efb3c0675ea60173971f07
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-11-23 19:11:58 +0800

    noPackageJSON args for npm-shell.nix

--- nix/npm-shell.nix
+++ nix/npm-shell.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   mkShell,
   importNpmLock,
   nodejs,
@@ -8,16 +9,23 @@
   emacs-lsp-booster,
   prettier,
 }:
-{ npmRoot }:
-mkShell {
-  packages = [
-    nodejs
-    importNpmLock.hooks.linkNodeModulesHook
-    typescript
-    typescript-language-server
-    bun
-    emacs-lsp-booster
-    prettier
-  ];
-  npmDeps = importNpmLock.buildNodeModules { inherit npmRoot nodejs; };
-}
+{
+  npmRoot,
+  noPackageJSON ? false, # NOTE: set this to true to just have a npm env to init your packge.
+}:
+mkShell (
+  {
+    packages = [
+      nodejs
+      typescript
+      typescript-language-server
+      bun
+      emacs-lsp-booster
+      prettier
+    ]
+    ++ (lib.optional (noPackageJSON == false) importNpmLock.hooks.linkNodeModulesHook);
+  }
+  // (lib.optionalAttrs (noPackageJSON == false) {
+    npmDeps = importNpmLock.buildNodeModules { inherit npmRoot nodejs; };
+  })
+)
