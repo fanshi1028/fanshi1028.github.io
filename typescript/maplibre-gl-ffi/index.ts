b2b867d202beb85028cb78ae4c08efe71a17bd01
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-11-24 23:05:23 +0800

    fixup! init FFI to maplibre

---
+++ typescript/maplibre-gl-ffi/index.ts
@@ -0,0 +1,17 @@
+import { Map, Marker, type LngLatLike } from 'maplibre-gl'
+
+const createMap = (cid: string) =>
+  new Map({
+    container: cid,
+    style: 'https://tiles.openfreemap.org/styles/liberty',
+    zoom: 12,
+  })
+
+const addMarkerAndEaseToLocation = (lng: number, lat: number, mapLbre: Map) => {
+  const location: LngLatLike = [lng, lat]
+  new Marker().setLngLat(location).addTo(mapLbre)
+  mapLbre.easeTo({ center: location })
+}
+
+// @ts-ignore: HACK provide a variance to check this file is loaded
+window.maplibregl = { createMap, addMarkerAndEaseToLocation }
