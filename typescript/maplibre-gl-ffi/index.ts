536e42bb755c43f8427b765436d1d43767182a1d
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-11-25 17:23:15 +0800

    try

--- typescript/maplibre-gl-ffi/index.ts
+++ typescript/maplibre-gl-ffi/index.ts
@@ -1,20 +1,20 @@
 import { Map, Marker, type LngLatLike } from 'maplibre-gl'
 
+const createMap = (cid: string) =>
+  new Map({
+    container: cid,
+    style: 'https://tiles.openfreemap.org/styles/liberty',
+    zoom: 12,
+  })
+
+const addMarkerAndEaseToLocation = (lng: number, lat: number, mapLbre: Map) => {
+  const location: LngLatLike = [lng, lat]
+  new Marker().setLngLat(location).addTo(mapLbre)
+  mapLbre.easeTo({ center: location })
+}
+
 declare global {
   var maplibregl_ffi: unknown
 }
 
-globalThis.maplibregl_ffi = {
-  createMap(cid: string) {
-    return new Map({
-      container: cid,
-      style: 'https://tiles.openfreemap.org/styles/liberty',
-      zoom: 12,
-    })
-  },
-  addMarkerAndEaseToLocation(lng: number, lat: number, mapLbre: Map) {
-    const location: LngLatLike = [lng, lat]
-    new Marker().setLngLat(location).addTo(mapLbre)
-    mapLbre.easeTo({ center: location })
-  },
-}
+globalThis.maplibregl_ffi = { createMap, addMarkerAndEaseToLocation }
