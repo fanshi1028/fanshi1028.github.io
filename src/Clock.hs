309143ac63b7f8e74df5ec8e2426f5db9cfa4286
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-11-26 10:08:18 +0800

    factor out Clock.View

--- src/Clock.hs
+++ src/Clock.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Clock (ClockDoneMessage (ClockDoneMessage), clockComponent) where
 
+import Clock.View
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
@@ -10,20 +10,9 @@ import Data.Aeson hiding ((.=))
 import Data.Time
 import GHC.Generics
 import Miso
-import Miso.Html
-import Miso.Html.Property
 import Miso.Lens
 import Miso.String
-import Miso.Svg.Element
-import Miso.Svg.Property hiding (path_)
 import Prelude hiding (error)
-
-data Model = Model
-  { _active :: Bool,
-    _timeLeft :: NominalDiffTime,
-    _lastTick :: Maybe UTCTime
-  }
-  deriving (Eq)
 
 active :: Lens Model Bool
 active = lens _active $ \record x -> record {_active = x}
@@ -34,12 +23,10 @@ lastTick = lens _lastTick $ \record x -> record {_lastTick = x}
 timeLeft :: Lens Model NominalDiffTime
 timeLeft = lens _timeLeft $ \record x -> record {_timeLeft = x}
 
-data Action = Tick UTCTime | Start | Stop | End
-
 data StopWatchTickSub = StopWatchTickSub
 
 instance ToMisoString StopWatchTickSub where
-  toMisoString _ = "StopWatchTickSub"
+  toMisoString _ = ms "StopWatchTickSub"
 
 data ClockDoneMessage = ClockDoneMessage deriving (Eq, Generic, ToJSON, FromJSON)
 
@@ -65,26 +52,6 @@ updateModel = \case
     lastTick .= Nothing
     stopSub StopWatchTickSub
 
-viewModel :: Model -> View Model Action
-viewModel m =
-  div_ [class_ "flex flex-col items-center justify-center gap-4 p-6 h-full"] $
-    [ p_ [class_ "text-neutral-300 text-7xl sm:text-8xl font-mono font-semibold"] [text . ms . formatTime defaultTimeLocale "%M:%00ES" $ m ^. timeLeft],
-      div_ [class_ "grid grid-cols-2 place-items-center w-full items-center"] $
-        [ let (primaryAction, primarybuttonText) =
-                if m ^. active
-                  then (Stop, "Stop")
-                  else (Start, "Start")
-              buttonCls = "bg-neutral-300 text-neutral-600 text-3xl sm:text-4xl rounded px-4 sm:px-6 py-2 sm:py-3 shadow-inner shadow-neutral-800 hover:animate-wiggle"
-           in button_ [onClick primaryAction, class_ buttonCls] [primarybuttonText],
-          button_
-            [onClick End, class_ "self-center w-fit mx-auto hover:animate-wiggle"]
-            [ svg_
-                [class_ "fill-neutral-300 size-16 sm:size-20", xmlns_ "http://www.w3.org/2000/svg", viewBox_ "0 0 24 24"]
-                [path_ [d_ "M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z"]]
-            ]
-        ]
-    ]
-
 clockComponent :: Bool -> NominalDiffTime -> Component parent Model Action
 clockComponent active' currentPomodoroTime =
-  (component (Model active' currentPomodoroTime Nothing) updateModel viewModel) {initialAction = Just Clock.Start}
+  (component (Model active' currentPomodoroTime Nothing) updateModel viewModel) {initialAction = Just Start}
---
+++ src/Clock/View.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Clock.View (viewModel, Model (..), Action (..)) where
+
+import Data.Time
+import Miso
+import Miso.Html
+import Miso.Html.Property
+import Miso.Svg.Element
+import Miso.Svg.Property hiding (path_)
+
+data Model = Model
+  { _active :: Bool,
+    _timeLeft :: NominalDiffTime,
+    _lastTick :: Maybe UTCTime
+  }
+  deriving (Eq)
+
+data Action = Tick UTCTime | Start | Stop | End
+
+viewModel :: Model -> View Model Action
+viewModel (Model active timeLeft _) =
+  div_ [class_ "flex flex-col items-center justify-center gap-4 p-6 h-full"] $
+    [ p_ [class_ "text-neutral-300 text-7xl sm:text-8xl font-mono font-semibold"] [text . ms $ formatTime defaultTimeLocale "%M:%00ES" timeLeft],
+      div_ [class_ "grid grid-cols-2 place-items-center w-full items-center"] $
+        [ let (primaryAction, primarybuttonText) =
+                if active
+                  then (Stop, "Stop")
+                  else (Start, "Start")
+              buttonCls = "bg-neutral-300 text-neutral-600 text-3xl sm:text-4xl rounded px-4 sm:px-6 py-2 sm:py-3 shadow-inner shadow-neutral-800 hover:animate-wiggle"
+           in button_ [onClick primaryAction, class_ buttonCls] [primarybuttonText],
+          button_
+            [onClick End, class_ "self-center w-fit mx-auto hover:animate-wiggle"]
+            [ svg_
+                [class_ "fill-neutral-300 size-16 sm:size-20", xmlns_ "http://www.w3.org/2000/svg", viewBox_ "0 0 24 24"]
+                [path_ [d_ "M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z"]]
+            ]
+        ]
+    ]
