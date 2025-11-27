fc1673b0f1b1c00eba0adcf2c964eb0d138613d8
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-11-27 08:04:34 +0800

    consoleLog should not be DataSource

--- src/Dashboard/DataSource/JSM.hs
+++ src/Dashboard/DataSource/JSM.hs
@@ -2,10 +2,11 @@
 
 module Dashboard.DataSource.JSM where
 
-import Control.Exception (SomeException, toException)
+import Control.Exception (SomeException)
 import Data.Aeson
 import Data.Aeson.Encode.Pretty
 import Data.Csv
+import Data.Functor
 import Data.Hashable
 import Data.Text hiding (concat, elem, foldl', foldr, reverse, show)
 import Data.Text qualified as T
@@ -13,15 +14,15 @@ import Data.Text.Lazy.Builder
 import Data.Typeable
 import Data.Vector hiding ((!))
 import Haxl.Core
+import Haxl.Core.Monad
 import Language.Javascript.JSaddle
-import Miso hiding (Decoder, URI, defaultOptions, on)
+import Miso hiding (Decoder, URI, consoleLog, defaultOptions, on)
 import Network.URI
 import Utils.Fetch
 import Utils.Fetch.CSV
 import Utils.Serialise
 
 data JSMAction a where
-  ConsoleLog :: MisoString -> JSMAction ()
   FetchJSON :: URI -> JSMAction SerialisableValue
   FetchCSV :: forall a. (FromRecord a) => URI -> JSMAction (Vector a)
   FetchText :: URI -> JSMAction StrictText
@@ -38,10 +39,9 @@ instance StateKey JSMAction where
 instance Hashable (JSMAction a) where
   hashWithSalt s =
     hashWithSalt @Int s . \case
-      ConsoleLog str -> s `hashWithSalt` (0 :: Int) `hashWithSalt` fromMisoString @StrictText str
-      FetchJSON uri -> s `hashWithSalt` (1 :: Int) `hashWithSalt` uriToString id uri ""
-      FetchCSV uri -> s `hashWithSalt` (2 :: Int) `hashWithSalt` uriToString id uri ""
-      FetchText uri -> s `hashWithSalt` (3 :: Int) `hashWithSalt` uriToString id uri ""
+      FetchJSON uri -> s `hashWithSalt` (0 :: Int) `hashWithSalt` uriToString id uri ""
+      FetchCSV uri -> s `hashWithSalt` (1 :: Int) `hashWithSalt` uriToString id uri ""
+      FetchText uri -> s `hashWithSalt` (2 :: Int) `hashWithSalt` uriToString id uri ""
 
 instance DataSourceName JSMAction where
   dataSourceName _ = T.show . typeRepTyCon . typeRep $ Proxy @JSMAction
@@ -51,13 +51,12 @@ instance DataSource u JSMAction where
     where
       performJSM :: JSMAction a -> JSM (Either SomeException a)
       performJSM = \case
-        ConsoleLog str -> Right <$> Miso.consoleLog str
         FetchJSON uri -> fetchGetJSON Proxy uri
         FetchCSV uri -> fetchGetCSV uri
         FetchText uri -> fetchGetText uri
 
-consoleLog :: MisoString -> GenHaxl u w ()
-consoleLog = uncachedRequest . ConsoleLog
+consoleLog :: JSContextRef -> MisoString -> GenHaxl u w ()
+consoleLog jscontext = void . unsafeLiftIO . runJSaddle jscontext . (jsg "console" # "log")
 
-consoleLog' :: (ToJSON v) => v -> GenHaxl u w ()
-consoleLog' = uncachedRequest . ConsoleLog . ms . toLazyText . encodePrettyToTextBuilder
+consoleLog' :: (ToJSON v) => JSContextRef -> v -> GenHaxl u w ()
+consoleLog' jscontext = consoleLog jscontext . ms . toLazyText . encodePrettyToTextBuilder
