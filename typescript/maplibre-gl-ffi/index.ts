efb2681e9b2a8521049ecf227ffdb1f7c94574a3
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-12-06 00:07:21 +0800

    ts: init getDataURI

--- typescript/maplibre-gl-ffi/index.ts
+++ typescript/maplibre-gl-ffi/index.ts
@@ -25,11 +25,33 @@ const addMarkerAndEaseToLocation = (lng: number, lat: number, mapLbre: Map) => {
   mapLbre.easeTo({ center: location })
 }
 
+const getDataURI = (
+  data: GeoJSONSourceSpecification['data'],
+  callback: (data_url: string) => void
+) => {
+  if (typeof data !== 'string' && data.type == 'FeatureCollection') {
+    const data_url = data.features[0]?.properties?.['Data_url']
+    if (typeof data_url == 'string' && data_url != '') callback(data_url)
+  }
+}
+
 const renderUVIndexGeoJSON = (
   map: Map,
   id: string,
   data: GeoJSONSourceSpecification['data']
 ) => {
+  if (typeof data !== 'string') {
+    if (data.type == 'FeatureCollection') {
+      const hi = data.features[0]?.properties
+      // Data_url : "https://data.weather.gov.hk/weatherAPI/hko_data/csdi/dataset/latest_15min_uvindex_csdi_0.csv"
+      // GmlID : "latest_15min_uvindex.1"
+      // OBJECTID : 1 Region_en : "Hong Kong"
+      // Region_sc : "香港"
+      // Region_uc : "香港"
+      // SHAPE_Length : 2.23
+      // gml_id : "latest_15min_uvindex.0"
+    }
+  }
   map.addSource(id, { type: 'geojson', data }).addLayer({
     id,
     source: id,
@@ -46,5 +68,6 @@ globalThis.maplibregl_ffi = {
   createMap,
   addMarkerAndEaseToLocation,
   renderUVIndexGeoJSON,
+  getDataURI,
   hssp7,
 }
