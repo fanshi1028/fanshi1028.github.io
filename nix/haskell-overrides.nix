9a728c3222ec20862e7c7855aea57b06cd6fc529
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2026-01-24 18:10:26 +0800

    fixup! dontcheck jsaddle-warp

--- nix/haskell-overrides.nix
+++ nix/haskell-overrides.nix
@@ -10,11 +10,13 @@ hself: hsuper: {
     sha256 = "sha256-/p9p/hBK4TsTR524n1i8tgsJwv7Vw+i288Ccmzb2bfI=";
   } { };
 
-  jsaddle-warp = haskell.lib.dontCheck hself.callHackageDirect {
-    pkg = "jsaddle-warp";
-    ver = "0.9.9.5";
-    sha256 = "sha256-Wv1upgYJP4zHbVtamRLCWNNAcI1NOLQJ4VpR7QDceOY=";
-  } { };
+  jsaddle-warp = haskell.lib.dontCheck (
+    hself.callHackageDirect {
+      pkg = "jsaddle-warp";
+      ver = "0.9.9.5";
+      sha256 = "sha256-Wv1upgYJP4zHbVtamRLCWNNAcI1NOLQJ4VpR7QDceOY=";
+    } { }
+  );
 
   miso = lib.pipe hsuper.miso (with haskell.lib.compose; [ (enableCabalFlag "template-haskell") ]);
 
