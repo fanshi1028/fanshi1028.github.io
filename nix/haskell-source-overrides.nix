3a8bf775bbd433c1d2c39fbd52ef5a12f6d3e471
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2026-01-26 13:09:26 +0800

    fix runtime duplicate C symbols when ghci wasm in the browser

--- nix/haskell-source-overrides.nix
+++ nix/haskell-source-overrides.nix
@@ -8,5 +8,12 @@
     sha256 = "sha256-FXx/Cm8/UsXgXSwjFrt6hgpYzjQutOhJvopYFGQLjKs=";
   };
 
+  ieee754 = fetchFromGitHub {
+    owner = "fanshi1028";
+    repo = "hs-ieee754";
+    rev = "01ddbabf11f5fc143503fc134d740668c6a90294";
+    sha256 = "sha256-d1zUtplbcsHKiBOt1q4iOtxcjGagJbOAmuNRwzecSrM=";
+  };
+
   hashtables = "1.4.2";
 }
