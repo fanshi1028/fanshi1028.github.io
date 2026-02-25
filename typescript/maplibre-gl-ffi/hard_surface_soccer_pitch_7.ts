3105e2591607a48ec1f02ace2d9e3085295c2169
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2026-02-25 09:04:57 +0800

    chore

--- typescript/maplibre-gl-ffi/hard_surface_soccer_pitch_7.ts
+++ typescript/maplibre-gl-ffi/hard_surface_soccer_pitch_7.ts
@@ -36,19 +36,17 @@ export const hard_surface_soccer_pitch_7 = {
   ) {
     if (features === null) {
       processCoords?.(hssp7_data, (coords) => {
-        features = coords.filter(isNotNull).map((d, i) => {
-          return {
-            type: 'Feature' as const,
-            geometry: {
-              type: 'Point' as const,
-              coordinates: LngLat.convert(d).toArray(),
-            },
-            properties: {
-              title: hssp7_data[i]?.Name_en,
-              // title: hssp7[i]?.Name_cn,
-            },
-          }
-        })
+        features = coords.filter(isNotNull).map((d, i) => ({
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: LngLat.convert(d).toArray(),
+          },
+          properties: {
+            title: hssp7_data[i]?.Name_en,
+            // title: hssp7[i]?.Name_cn,
+          },
+        }))
         features.length == 0
           ? console.error(
               'hard_surface_soccer_pitch_7 toggleLayer: feature is empty / all null.'
