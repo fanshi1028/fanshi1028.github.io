4f81f10b218e1aff63b419cfd7ccd46aa59a6739
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2026-02-27 20:38:50 +0800

    chore

--- src/Component/Dashboard/View.hs
+++ src/Component/Dashboard/View.hs
@@ -332,53 +332,51 @@ view9DayWeatherForecast
       [ h2_ [class_ "sr-only"] [text "Weather Forecast"],
         case weatherForecasts !? fromIntegral timeSliderValue of
           Nothing -> text . ms $ "impossible timeSliderValue: " <> show timeSliderValue
-          Just forecast -> viewWeatherForecast forecast
-      ]
-    where
-      viewWeatherForecast
-        ( WeatherForecast
-            forecastDate
-            weekDay
-            forecastWind
-            forecastWeather
-            forecastTempInterval
-            forecastRHInterval
-            psr
-            forecastIcon
-          ) =
-          div_ [class_ "flex flex-col gap-2"] $
-            [ div_ [class_ "group relative"] $
-                [ h2_ [class_ "text-lg"] [text . ms $ show weekDay <> " " <> show forecastDate],
-                  makePopover . text . ms $ "Updated " <> showRelativeTime mCurrentTime updateTime
-                ],
-              div_ [] $
-                [ text . ms $ case (lowerBound forecastTempInterval, upperBound forecastTempInterval) of
-                    (Finite lb, Finite ub) -> "🌡 " <> show (toDegreeCelsiusAbsolute lb) <> " - " <> show (toDegreeCelsiusAbsolute ub) <> " °C"
-                    _ -> "impossible: unexpected temperature interval for forecast data"
-                ],
-              div_ [] $
-                [ text . ms $ case (lowerBound forecastRHInterval, upperBound forecastRHInterval) of
-                    (Finite lb, Finite ub) -> "💧 " <> show (lb /~ percent) <> " - " <> pack (showIn percent ub)
-                    _ -> "impossible: unexpected relative humidity interval for forecast data"
-                ],
-              div_ [class_ "flex flex-row gap-2 relative group"] $
-                [ "🌧",
-                  input_
-                    [ type_ "range",
-                      min_ . ms . show . fromEnum $ minBound @ProbabilityOfSignificantRain,
-                      max_ . ms . show . fromEnum $ maxBound @ProbabilityOfSignificantRain,
-                      value_ . ms . show $ fromEnum psr,
-                      disabled_
+          Just
+            ( WeatherForecast
+                forecastDate
+                weekDay
+                forecastWind
+                forecastWeather
+                forecastTempInterval
+                forecastRHInterval
+                psr
+                forecastIcon
+              ) ->
+              div_ [class_ "flex flex-col gap-2"] $
+                [ div_ [class_ "group relative"] $
+                    [ h2_ [class_ "text-lg"] [text . ms $ show weekDay <> " " <> show forecastDate],
+                      makePopover . text . ms $ "Updated " <> showRelativeTime mCurrentTime updateTime
                     ],
-                  makePopover . text . ms $ show psr <> " probability of significant rain"
-                ],
-              case forecastWind of
-                "" -> div_ [class_ "hidden"] []
-                _ -> div_ [] [text . ms $ forecastWind],
-              case forecastWeather of
-                "" -> div_ [class_ "hidden"] []
-                _ -> div_ [] [text . ms $ forecastWeather]
-            ]
+                  div_ [] $
+                    [ text . ms $ case (lowerBound forecastTempInterval, upperBound forecastTempInterval) of
+                        (Finite lb, Finite ub) -> "🌡 " <> show (toDegreeCelsiusAbsolute lb) <> " - " <> show (toDegreeCelsiusAbsolute ub) <> " °C"
+                        _ -> "impossible: unexpected temperature interval for forecast data"
+                    ],
+                  div_ [] $
+                    [ text . ms $ case (lowerBound forecastRHInterval, upperBound forecastRHInterval) of
+                        (Finite lb, Finite ub) -> "💧 " <> show (lb /~ percent) <> " - " <> pack (showIn percent ub)
+                        _ -> "impossible: unexpected relative humidity interval for forecast data"
+                    ],
+                  div_ [class_ "flex flex-row gap-2 relative group"] $
+                    [ "🌧",
+                      input_
+                        [ type_ "range",
+                          min_ . ms . show . fromEnum $ minBound @ProbabilityOfSignificantRain,
+                          max_ . ms . show . fromEnum $ maxBound @ProbabilityOfSignificantRain,
+                          value_ . ms . show $ fromEnum psr,
+                          disabled_
+                        ],
+                      makePopover . text . ms $ show psr <> " probability of significant rain"
+                    ],
+                  case forecastWind of
+                    "" -> div_ [class_ "hidden"] []
+                    _ -> div_ [] [text $ ms forecastWind],
+                  case forecastWeather of
+                    "" -> div_ [class_ "hidden"] ["No forecastWeather"]
+                    _ -> div_ [] [text $ ms forecastWeather]
+                ]
+      ]
 
 viewModel :: Model -> View Model Action
 viewModel (Model mCurrentTime timeSliderValue mELocation mFocusedDistrict mCurrentWeatherReport mLocalWeatherForecast m9DayWeatherForecast rainfallDisplayMode ifDisplayTemperature) =
